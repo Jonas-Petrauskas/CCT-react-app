642f2f874709714d4046ade39207dac9cb3681e1
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2021-11-08 15:17:04 +0100

    data structure added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,63 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const data = [
+    {
+      title: "Build test task",
+      items: [
+        {
+          text: "Create repositor",
+        },
+        {
+          text: "Implement designs",
+        },
+        {
+          text: "Implement functionality",
+        },
+      ],
+    },
+    {
+      title: "Submit your test task",
+      items: [
+        {
+          text: "Open email client",
+        },
+        {
+          text: "Sent link information to",
+          email: "careers@cornercasetech.com",
+        },
+      ],
+    },
+    {
+      title: "Participate in tech interview",
+      items: [
+        {
+          text: "Talk with HR",
+        },
+        {
+          text: "Talk with Tech team",
+        },
+      ],
+    },
+    {
+      title: "Reciece anster",
+      items: [
+        {
+          text: "Create repositor",
+        },
+        {
+          text: "Receive answers",
+        },
+        {
+          text: "Start your IT career",
+        },
+      ],
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="app">
+      <header className="app-header"></header>
     </div>
   );
 }
