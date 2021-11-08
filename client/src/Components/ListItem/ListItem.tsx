2ee842ff2fb06cc6a4926f44bffe075f8fcb755b
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2021-11-08 16:08:40 +0100

    displayed all data information

--- client/src/Components/ListItem/ListItem.tsx
+++ client/src/Components/ListItem/ListItem.tsx
@@ -8,14 +8,23 @@ function ListItem({ listData, listNumber, open, setOpen }: ListItemProps) {
   return (
     <div className="list">
       <div className="section-container">
-        <div className="section-number"></div>
-        <p className="section-title"></p>
+        <div className="section-number">{listNumber}</div>
+        <p className="section-title">{listData.title}</p>
       </div>
       <div className="list-details">
         {open &&
           listData.items.map((detailsItem, index) => (
-            <div className="details-container">
+            <div className="details-container" key={index}>
               <CheckedIcon />
+              <p className="details-item">
+                {detailsItem.text}{" "}
+                <a
+                  href="mailto:careers@cornercasetech.com"
+                  className="details-emails"
+                >
+                  {detailsItem.email}
+                </a>
+              </p>
             </div>
           ))}
       </div>
